e445bcd21ffb41e2c40b2936be0dc4c0453ccf5e
Edson Samuel Jr <samuedson@gmail.com>
2015-01-15 11:25:40 -0300

    working with data lists and selects

--- controller.js
+++ controller.js
@@ -2,4 +2,31 @@ app.controller("controller", function($scope) {
   $scope.intro = "Tis' the freakin' understandable (and bindable) $scope!";
   $scope.nameLabel = "Name: ";
   $scope.nameValue = "";
+
+  $scope.selectedBrand = null;
+  $scope.selectedCar = null;
+
+  $scope.carLabel = "Selected vehicle: ";
+  $scope.vehicles = [
+    {
+      id: 0,
+      brand: 'Chevrolet',
+      model: ['Celta', 'Onix', 'Cruze']
+    },
+    {
+      id: 1,
+      brand: 'Fiat',
+      model: ['Uno', 'Palio', 'Punto', 'Bravo']
+    },
+    {
+      id: 2,
+      brand: 'Ford',
+      model: ['Ka', 'Fiesta', 'Focus']
+    },
+    {
+      id: 3,
+      brand: 'Volkswagen',
+      model: ['Up', 'Gol', 'Fox', 'Golf']
+    }
+  ];
 });
